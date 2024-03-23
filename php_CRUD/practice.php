850936e4d148c784a5f61500a221759278addbb3
KapilSinghThakuri <kapilsinghthakuri33@gmail.com>
2024-03-23 09:59:19 +0545

    phpWithAjax

--- php_CRUD/practice.php
+++ php_CRUD/practice.php
@@ -7,21 +7,25 @@ $dbname = "phpCRUD";
 // Create connections with database
 $conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection Failed: " . mysqli_connect_error());
 
-$query = "SELECT * FROM student";
+$query = "SELECT * FROM students";
+$result = mysqli_query($conn, $query);
+echo "<pre>";
+print_r($result);
+$num_rows = mysqli_num_rows($result);
+echo "Number of rows : $num_rows";
+// try {
+//     $result = mysqli_query($conn, $query);
 
-try {
-    $result = mysqli_query($conn, $query);
+//     if (!$result) {
+//         throw new Exception(mysqli_error($conn));
+//     }
 
-    if (!$result) {
-        throw new Exception(mysqli_error($conn));
-    }
-
-    echo "<pre>";
-    print_r(mysqli_fetch_all($result));
-    echo "</pre>";
-} catch (Exception $e) {
-    echo "Query Failed: " . $e->getMessage();
-}
+//     echo "<pre>";
+//     print_r(mysqli_fetch_all($result));
+//     echo "</pre>";
+// } catch (Exception $e) {
+//     echo "Query Failed: " . $e->getMessage();
+// }
 
 mysqli_close($conn);
 ?>
